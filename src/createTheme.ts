770ab22737c9a143c6484ea0bfc53edce5ac5563
Davide Bruner <davidebruner@hotmail.it>
2024-03-20 18:18:39 +0100

    feat: update createTheme function

--- src/createTheme.ts
+++ src/createTheme.ts
@@ -4,24 +4,5 @@ import { CreateThemeOptions, Theme } from "./types";
 import defaultTheme from "./defaultTheme";
 
 export const createTheme = <C>(theme: CreateThemeOptions<C> = {}): Theme => {
-  return {
-    ...theme,
-    ...deepmerge(
-      {
-        spacing: defaultTheme.spacing,
-        fontSizes: defaultTheme.fontSizes,
-        platformColors: defaultTheme.platformColors,
-      },
-      {
-        colors: theme?.colors
-          ? deepmerge(theme?.colors, defaultTheme.colors)
-          : defaultTheme.colors,
-        colorScheme: theme.colorScheme || defaultTheme.colorScheme,
-        spacing: theme.spacing || {},
-        components: theme.components || {},
-        primaryShade: theme.primaryShade || defaultTheme.primaryShade,
-        fontSizes: theme?.fontSizes || {},
-      }
-    ),
-  };
+  return deepmerge(defaultTheme, theme);
 };
